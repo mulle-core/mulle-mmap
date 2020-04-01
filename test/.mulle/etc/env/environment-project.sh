3602a35e32869dcb45826037784dd9245ef57651
Nat! <nat@mulle-kybernetik.com>
2020-04-01 20:36:41 +0200

    add missing files

---
+++ test/.mulle/etc/env/environment-project.sh
@@ -0,0 +1,48 @@
+#
+#
+#
+export PROJECT_NAME="mulle-mmap"
+
+
+#
+#
+#
+export PROJECT_IDENTIFIER="mulle_mmap"
+
+
+#
+#
+#
+export PROJECT_DOWNCASE_IDENTIFIER="mulle_mmap"
+
+
+#
+#
+#
+export PROJECT_UPCASE_IDENTIFIER="MULLE_MMAP"
+
+
+#
+#
+#
+export PROJECT_TYPE="none"
+
+
+#
+#
+#
+export PROJECT_LANGUAGE="c"
+
+
+#
+#
+#
+export PROJECT_DIALECT="c"
+
+
+#
+#
+#
+export PROJECT_EXTENSIONS="c"
+
+
